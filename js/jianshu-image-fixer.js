e53da3288c1ecdc1e5a2ab2d37d27e2c86871fa7
Cao Yong <edencaox@gmail.com>
2019-05-06 18:17:38 +0800

    Site updated: 2019-05-06 18:17:38

--- js/jianshu-image-fixer.js
+++
@@ -1,7 +0,0 @@
-// 因为过去的文章是写在简书上面的，迁移过来写了个脚本直接扒下来的
-// 此脚本主要是为了解决扒下来的文章的图片的问题
-document
-  .querySelectorAll('.image-package .image-view img')
-  .forEach(function(image) {
-    image.src = image.getAttribute('data-original-src');
-  })
